82af33f63b030f45c452760bf7326ec6304200e9
Oleksandr Pasternak <pasternak.job@gmail.com>
2023-11-08 13:04:55 +0100

    Add files via upload

--- mysqlycsv/exportar.php
+++ mysqlycsv/exportar.php
@@ -1,14 +1,11 @@
 <?php
-// Conexión a la base de datos
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "alumnos";
 
-// Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
